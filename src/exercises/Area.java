aa0af9f2662dec22c7d0a7cc3add84398da630d9
Michael Sisay <ashenafimichael25@gmail.com>
2019-11-07 19:48:51 -0600

    done with area

--- src/exercises/Area.java
+++ src/exercises/Area.java
@@ -1,14 +1,39 @@
 package exercises;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("enter the length of rectangle:");
-        double length = in.nextDouble();
-        System.out.println("enter the width of rectangle:");
-        double width = in.nextDouble();
-        double Area = length * width ;
-        System.out.println("The area of the rectangle is "+ Area +".");
+        double radius = valid();
+
+
+        double Area = Math.PI * Math.pow(radius,2);
+        System.out.println("The area of the cirlce is " + Area + ".");
     }
+
+    private static double valid (){
+        Scanner in = new Scanner(System.in);
+        boolean validation1 = true;
+
+        String error = "value must be above 0";
+        double radius = -2 ;
+
+        do {
+            try {
+                System.out.println("enter the radius : ");
+                radius = in.nextDouble();
+                if(radius <=0){
+                    System.out.println(error);
+                }else{
+                    validation1 = false;
+                }
+            }catch (InputMismatchException e){
+                System.out.println("invalid output");
+            }
+
+            in.nextLine();
+        }while(validation1);
+        return radius;
+    }
+
 }
